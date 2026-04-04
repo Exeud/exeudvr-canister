249ceaf77fb352dc2bc6d7b060a75a89163abc87
Will Guest <will@exeud.com>
2026-04-04 16:29:06 +0100

    update mops, target pages

--- webpack.config.js
+++ webpack.config.js
@@ -121,15 +121,13 @@ module.exports = {
   ],
   // proxy /api to port 4943 during development
   devServer: {
-    proxy: {
-      "/api": {
+    proxy: [
+      {
+        context: ["/api"],
         target: "http://127.0.0.1:4943",
         changeOrigin: true,
-        pathRewrite: {
-          "^/api": "/api",
-        },
       },
-    },
+    ],
     static: path.resolve(__dirname, "src", "assets"),
     hot: true,
     watchFiles: [path.resolve(__dirname, "src")],
